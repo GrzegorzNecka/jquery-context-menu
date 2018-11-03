12fa726463efa86cad136af8b03ca76355359b70
Grzegorz Nęcka <strzypior@gmail.com>
2018-11-03 19:03:20 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+(function ($) {
+    $(document).ready(function () {
+
+        var logo = $(".logo"),
+            body = $("body"),
+            ul = null;
+
+
+        /*---------------------
+            CREATE MENU
+        ---------------------*/
+
+        function createContextMenu(config) {
+            var ul = $("<ul></ul>", {
+                "id": "contextmenu",
+                "class": "contextmenu",
+                "css": {
+                    "display": "none"
+                },
+                on: {
+                    click: function (e) {
+                        e.stopPropagation();
+                    }
+                }
+            });//.ul
+
+            $.each(config, function (key, val) {
+
+                var li = $("<li></li>", {
+                    "class": "contextmenu__item"
+                });
+
+                var a = $("<a></a>", {
+                    "class": "contextmenu__link",
+                    "text": key,
+                    "href": val
+                });
+
+                li.append(a).appendTo(ul)
+
+            });//.each
+            
+            body.append(ul);
+            
+            return ul
+        }//.createContextMenu 
+
+
+        /*---------------------
+                OPEN MENU
+        ---------------------*/
+        
+        var config = {
+            "Logo w formie SVG": "#svg",
+            "Logo w formacie PNG": "#png",
+            "Zip z logo": "#zip"
+            };
+        
+        //contextmenu = dom event
+        logo.on("contextmenu", function (e) {
+            e.preventDefault();
+   
+//            var ul = $('ul#contextmenu')
+            
+            ul = ul || createContextMenu(config);
+            
+            ul.css({
+                display: "",
+                top: e.pageY,
+                left: e.pageX
+            }); //.css
+
+            
+                /*---------------------
+                    CLOSE MENU
+                ---------------------*/
+           
+            $(document).on("click", function (e) {
+            ul.css("display", "none");
+            });
+       
+        });
+
+
+    }); //ready
+})(jQuery);
